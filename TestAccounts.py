ccf1642c5abebcee74b05077bf90ccb5a118b5fa
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-04-08 15:44:20 -0500

    removed uatomtion

--- TestAccounts.py
+++ TestAccounts.py
@@ -6,6 +6,3 @@ class TestAccounts(unittest.TestCase):
     def test_httpStatus(self):
         httpStatusCheck = Accounts(httpStatus)
         self.assertEqual(httpStatusCheck, 200)
-
-if __name__ == '__main__':
-    unittest.main()
